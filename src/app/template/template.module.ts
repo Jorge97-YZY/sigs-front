78246940ef6b63ce3b8d0112e71cd3d0b900c184
Jorge Manuel <jorgeyeez@gmail.com>
2021-12-02 12:49:18 +0100

    Choose: Criação dos componentes do Template e Home

---
+++ src/app/template/template.module.ts
@@ -0,0 +1,25 @@
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+
+
+
+@NgModule({
+  declarations: [
+    NavbarComponent,
+    SidebarComponent,
+    FooterComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports:[
+    NavbarComponent,
+    SidebarComponent,
+    FooterComponent
+
+  ]
+})
+export class TemplateModule { }
